334669b01aa1893071392cf0f1dcdd15ac77c1ec
Amy Jordan <amy_jordan@live.co.uk>
2019-06-03 11:09:14 +0100

    ProductGridModel #getProducts returns array of products

--- spec/ProductGridModelSpec.js
+++ spec/ProductGridModelSpec.js
@@ -31,5 +31,19 @@ describe("ProductGridModel", function() {
     it("returns empty array when no ProductModels have been created", function() {
       expect(productGridModel.getProducts()).toEqual([])
     })
+
+    it("returns array of ProductModels when ProductModels have been created", function() {
+      productGridModel.add({productID: 1,
+                            title: "Test product model",
+                            imageURL: "https://www.johnlewis.com/",
+                            price: 100.00})
+      productGridModel.add({productID: 2,
+                            title: "Test product model 2",
+                            imageURL: "https://www.johnlewis.com/",
+                            price: 200.00})
+      expect(productGridModel.getProducts()[0]).toBeDefined()
+      expect(productGridModel.getProducts()[1]).toBeDefined()
+      expect(productGridModel.getProducts()[2]).not.toBeDefined()
+    })
   })
 })
