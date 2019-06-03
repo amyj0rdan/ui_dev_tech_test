2133030527de9af87a7045ad69372e4a98b761d7
Amy Jordan <amy_jordan@live.co.uk>
2019-06-03 10:56:05 +0100

    ProductGridModel #add can create multiple ProductModels

--- spec/ProductGridModelSpec.js
+++ spec/ProductGridModelSpec.js
@@ -14,5 +14,16 @@ describe("ProductGridModel", function() {
                             price: 100.00})
       expect(productGridModel._products.length).toEqual(1)
     })
+    it("can create multiple ProductModels and add to products", function() {
+      productGridModel.add({productID: 1,
+                            title: "Test product model",
+                            imageURL: "https://www.johnlewis.com/",
+                            price: 100.00})
+      productGridModel.add({productID: 2,
+                            title: "Test product model 2",
+                            imageURL: "https://www.johnlewis.com/",
+                            price: 200.00})
+      expect(productGridModel._products.length).toEqual(2)
+    })
   })
 })
