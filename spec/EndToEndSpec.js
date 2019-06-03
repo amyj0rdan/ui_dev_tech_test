8ccb0266fa3323c9d957cdbd7c505f2a03b64466
Amy Jordan <amy_jordan@live.co.uk>
2019-06-03 12:29:12 +0100

    Creates end-to-end (feature) test to ProductModel, ProductGridModel and ProductGridView To check interactions between models without mocks works correctly

---
+++ spec/EndToEndSpec.js
@@ -0,0 +1,23 @@
+describe("End to end test", function() {
+
+  var productGridView;
+
+  beforeEach(function() {
+    var productGridModel = new ProductGridModel()
+    productGridModel.add({productID: 1,
+                          title: "Test product model",
+                          imageURL: "https://www.johnlewis.com/",
+                          price: 100.00})
+    productGridModel.add({productID: 2,
+                          title: "Test product model 2",
+                          imageURL: "https://www.johnlewis.com/",
+                          price: 200.00})
+    productGridView = new ProductGridView(productGridModel)
+  })
+
+  it("can return an HTML div", function() {
+    expect(productGridView.createHTML()).toEqual(`<div class="product-square" id="1"><img src="https://www.johnlewis.com/"><span id="product-title">Test product model</span><span id="product-price">£100.00</span></div><div class="product-square" id="2"><img src="https://www.johnlewis.com/"><span id="product-title">Test product model 2</span><span id="product-price">£200.00</span></div>`)
+  })
+
+
+})
