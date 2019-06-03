7c4afa948b06287ef6d37101485da4372cf7828e
Amy Jordan <amy_jordan@live.co.uk>
2019-06-03 12:41:59 +0100

    ProductGridView #createHTML returns divs for multiple items

--- spec/ProductGridViewSpec.js
+++ spec/ProductGridViewSpec.js
@@ -1,9 +1,7 @@
 describe("ProductGridView", function() {
 
-  var productGridView;
-
   beforeEach(function() {
-    function ProductGridModelMock() {
+    function ProductGridModelMockOneProduct() {
       this.getProducts = function() {
         productModelMocks = [{ getProductID: function() {
                                   return 1;
@@ -21,13 +19,52 @@ describe("ProductGridView", function() {
         return productModelMocks;
       }
     }
-    var productGridModelMock = new ProductGridModelMock()
-    productGridView = new ProductGridView(productGridModelMock)
+    function ProductGridModelMockTwoProducts() {
+      this.getProducts = function() {
+        productModelMocks = [{ getProductID: function() {
+                                  return 1;
+                                },
+                                getTitle: function() {
+                                  return "Mock product model";
+                                },
+                                getImageURL: function() {
+                                  return "https://www.johnlewis.com/"
+                                },
+                                getPrice: function() {
+                                  return 100;
+                                }
+                              },
+                              { getProductID: function() {
+                                  return 2;
+                                },
+                                getTitle: function() {
+                                  return "Mock product model 2";
+                                },
+                                getImageURL: function() {
+                                  return "https://www.johnlewis.com/"
+                                },
+                                getPrice: function() {
+                                  return 200;
+                                }
+                              }
+                            ]
+        return productModelMocks;
+      }
+    }
+
+    var productGridModelMockOneProduct = new ProductGridModelMockOneProduct()
+    productGridViewOneProduct = new ProductGridView(productGridModelMockOneProduct)
+
+    var productGridModelMockTwoProducts = new ProductGridModelMockTwoProducts()
+    productGridViewTwoProducts = new ProductGridView(productGridModelMockTwoProducts)
   })
 
   describe("#createHTML", function() {
-    it("returns an HTML div", function() {
-      expect(productGridView.createHTML()).toEqual(`<div class="product-square" id="1"><img src="https://www.johnlewis.com/"><span id="product-title">Mock product model</span><span id="product-price">£100.00</span></div>`)
+    it("returns an HTML div for one product", function() {
+      expect(productGridViewOneProduct.createHTML()).toEqual(`<div class="product-square" id="1"><img src="https://www.johnlewis.com/"><span id="product-title">Mock product model</span><span id="product-price">£100.00</span></div>`)
+    })
+    it("returns an HTML div for two products", function() {
+      expect(productGridViewTwoProducts.createHTML()).toEqual(`<div class="product-square" id="1"><img src="https://www.johnlewis.com/"><span id="product-title">Mock product model</span><span id="product-price">£100.00</span></div><div class="product-square" id="2"><img src="https://www.johnlewis.com/"><span id="product-title">Mock product model 2</span><span id="product-price">£200.00</span></div>`)
     })
   })
 })
