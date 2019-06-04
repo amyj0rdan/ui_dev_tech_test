bfd36f078a49dc9a9dfbfc21570c4fc368c2fcef
Amy Jordan <amy_jordan@live.co.uk>
2019-06-04 14:48:05 +0100

    Create ProductIndividualModel, taking relevant API parameters

---
+++ src/ProductIndividualModel.js
@@ -0,0 +1,10 @@
+function ProductIndividualModel({title, media, price, productInformation,
+                                displaySpecialOffer, includedServices, code}) {
+  this._title = title;
+  this._media = media;
+  this._price = price;
+  this._productInformation = productInformation;
+  this._displaySpecialOffer = displaySpecialOffer;
+  this._includedServices = includedServices;
+  this._code = code;
+}
