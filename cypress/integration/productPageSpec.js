341524c1faf2e87db397f7789fe4681883f0e528
Amy Jordan <amy_jordan@live.co.uk>
2019-06-06 10:37:58 +0100

    User see product code and product information when clicking through to invidiual product

--- cypress/integration/productPageSpec.js
+++ cypress/integration/productPageSpec.js
@@ -1,7 +1,19 @@
 describe("Product Page", function() {
-  it("contains information on one dishwasher, including product price", function() {
+
+  beforeEach(function() {
     cy.visit("http://localhost:8080/")
     cy.get("#products").children().first().click()
+  })
+
+  it("contains dishwasher product price", function() {
     cy.get(".product-price").should("contain", "£")
+  })
+
+  it("contains dishwasher product code", function() {
+    cy.get(".product-code").should("contain", "Product code: ")
+  })
+
+  it("contains product information", function() {
+    cy.get(".product-information").should("be.visible")
   })
 })
